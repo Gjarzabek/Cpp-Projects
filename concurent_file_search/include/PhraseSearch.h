ff0bd7a9b8c3c26e427e53ed9c5110bebd550674
Grzegorz Jarząbek <46360840+Gjarzabek@users.noreply.github.com>
2019-05-19 22:14:43 +0200

    Update PhraseSearch.h

--- concurent_file_search/include/PhraseSearch.h
+++ concurent_file_search/include/PhraseSearch.h
@@ -3,7 +3,7 @@
 
 
 /**
-    Wspolbiezna przeszukiwarka plików
+    Wspolbiezna przeszukiwarka plik�w
 
     Program wczytuje fraze/slowo @szukana a nastepnie
     przeszukuje wszystkie piliki .txt o nazwach na wejsciu programu
@@ -12,6 +12,9 @@
     nazwa pliku --> ilosc znalezionych @szukana
     1. l @nr_linni, c @nr kolumny.
     ..
+
+    do dodania:
+      opcja ktora zmienia fraze na inna
 */
 
 #include <iostream>
@@ -20,28 +23,35 @@
 #include <fstream>
 #include <mutex>
 #include <string>
-#include <pair>
+#include <utility>
 
 const short THREADS = 4;
 using namespace std;
 
 class PhraseSearch {
   private:
-    static short n_threads;
-    vector<thread> threads;
-    vector<fstream> files;
-    mutex load_perm;
-    mutex output_perm;
-    static string phrase;
+    short n_threads; // ilosc watkow
+    string phrase;   // szukana fraza
+    mutex load_perm;        // zamek do wczytu z cin
+    mutex output_perm;      // zamek do cout
+    void print_result(vector<pair<int, int>> & a, string & fn);
+    void searchfile();
 
   public:
-    PhraseSearch(int n = THREADS) n_threads(n) {
-      cout << "Phrase to search for: ";
-      cin >> phrase;
+    PhraseSearch() :n_threads(1), phrase("") {};
+    PhraseSearch(int n = THREADS, string ph = "") :n_threads(n), phrase(ph) {};
+    void Set_phrase(string & str) {
+      phrase = str;
     };
-    void searchfile(); /// simple thread function
-    void print_result();
+    void search_for() {
+      thread thrs[n_threads];
+      for (int i = 0; i < n_threads; ++i)
+        thrs[i] = thread();
+      for (int i = 0; i < n_threads; ++i)
+        thrs[i].join();
+    }
 };
 
 
 #endif // PHRASESEARCH_H
+
